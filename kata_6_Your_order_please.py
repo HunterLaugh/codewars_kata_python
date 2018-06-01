5bd81c8cbd60fcecae25bcaee57fad77d2c43f8f
hua <493418400@qq.com>
2018-06-01 16:25:10 +0800

    learn unittest and logging

---
+++ kata_6_Your_order_please.py
@@ -0,0 +1,55 @@
+'''
+Your order, please
+Your task is to sort a given string. Each word in the String will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input String is empty, return an empty String. The words in the input String will only contain valid consecutive numbers.
+
+For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
+
+your_order("is2 Thi1s T4est 3a")
+[1] "Thi1s is2 3a T4est"
+
+'''
+
+import re
+import logging
+
+logger=logging.getLogger()
+logger.setLevel(30)
+
+def order(sentence):
+	if not sentence:
+		return ''
+		
+	num=[]
+	split=sentence.split()
+	for each in split:
+		temp=re.search(r'(\d+)',each)
+		num.append(int(temp.group()))
+	
+	i=0
+	while i<len(num)-1:
+		j=i+1
+		while j<len(num):
+			if num[i]>num[j]:
+				tp=num[i]
+				num[i]=num[j]
+				num[j]=tp
+				
+				sp=split[i]
+				split[i]=split[j]
+				split[j]=sp
+			j+=1
+		i+=1
+	
+	logging.info('num')
+	logging.info(num)
+	logging.info('split')
+	logging.info(split)
+	
+	return ' '.join(split)
+	
+# TEST CASE PASSED ALL  YOU CAN SEE IN THE TDD FOLDER
+order("is2 Thi1s T4est 3a")
